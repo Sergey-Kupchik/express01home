abe96c099368ea320f3894f134d1d4376e664468
kupchik <kastry4nik@gmail.com>
2022-10-12 09:33:34 -0400

    CI-2 added TS and dist

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3009
 
-app.get('/', (req, res) => {
-    res.send('Hello World))))')
+app.get('/', (req:any, res:any) => {
+
+    let helloWorld = 'Hello World!';
+    res.send(helloWorld)
 })
 
 app.listen(port, () => {
