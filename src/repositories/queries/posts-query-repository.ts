932f97c951618c8face5777a2c9a14a9952fe214
kupchik <kastry4nik@gmail.com>
2022-11-06 10:11:06 -0500

    CI-4 fixed dbCollection

--- src/repositories/queries/posts-query-repository.ts
+++ src/repositories/queries/posts-query-repository.ts
@@ -12,9 +12,9 @@ const postsQueryRepository = {
         return searchResult;
     },
     async getAllPostsFor1Blog(blogId: string, pageNumber: number, pageSize: number, sortBy: string, sortDirection: sortDirectionType): Promise<PostsOutputType> {
-        const totalCount: number = await dbCollections.blogs.estimatedDocumentCount();
+        // const totalCount: number = await dbCollections.blogs.estimatedDocumentCount();
         const filterParam = {blogId}
-        // const totalCount: number = await dbCollections.blogs.find(filterParam).count()
+        const totalCount: number = await dbCollections.posts.find(filterParam).count()
         const pagesCount: number = Math.ceil(totalCount / pageSize);
         const sortDirectionParam = sortDirection === sortDirectionEnum.asc ? 1 : -1;
         const skipItems: number = (pageNumber - 1) * pageSize;
