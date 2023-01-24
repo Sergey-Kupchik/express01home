6c2c91e55628cbeffa02f8add761db138937729d
kupchik <kastry4nik@gmail.com>
2023-01-23 22:25:40 -0500

    CI-9 last gess

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ app.use('/security', securityRouter);
 
 
 app.get('/', (req: Request, res: Response) => {
-    res.send(`1/22/23 at 10.08 pm`)
+    res.send(`1/22/23 at 10.25 pm`)
 })
 
 const startApp = async () => {
--- src/routes/security-router.ts
+++ src/routes/security-router.ts
@@ -32,13 +32,23 @@ securityRouter.delete('/devices/:devicesId',
     authRefreshToken,
     async (req: Request, res: Response) => {
         if (req.params.devicesId !== req.deviceId) {
-            const tokenInfo =  await tokensService.findRefreshTokenInfoByDeviceId(req.params.devicesId)
+            const tokenInfo = await tokensService.findRefreshTokenInfoByDeviceId(req.params.devicesId)
             if (tokenInfo===null) {
                 res.sendStatus(404)
                 return
             }
+            else if (tokenInfo.userId!==req.user!.accountData.id){
                 res.sendStatus(403)
                 return
+            } else {
+                const isDeleted: boolean = await tokensService.deleteAllTokensExceptCurrent(req.user!.accountData.id, req.params.devicesId)
+                if (!isDeleted) {
+                    res.sendStatus(404)
+                    return
+                }
+                res.sendStatus(204)
+                return
+            }
         } else {
             const isDeleted: boolean = await tokensService.deleteAllTokensExceptCurrent(req.user!.accountData.id, req.deviceId)
             if (!isDeleted) {
